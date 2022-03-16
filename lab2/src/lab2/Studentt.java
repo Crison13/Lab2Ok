c68c3f1670f7bfc71f9ec3d92e18f7ef348803c2
admin <admin@Bcalab4-m18>
2022-03-16 11:19:30 +0530

    frt

---
+++ lab2/src/lab2/Studentt.java
@@ -0,0 +1,18 @@
+package lab2;
+
+public class Studentt {
+	String name;
+	Studentt()
+	{
+		this.name = name;
+	}
+	void display()
+	{
+		System.out.println("Name: "+name);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
